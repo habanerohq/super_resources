b2e33eb7799482616631527019e9aff602c99435
Mark Ratjens <mark@ratjens.com>
2013-01-22 12:02:00 +1100

    get some actions_spec tests to pass

--- spec/controllers/actions_spec.rb
+++ spec/controllers/actions_spec.rb
@@ -1,158 +1,143 @@
 require 'spec_helper'
 
-describe 'In the context of generic RESTful-ness,' do
-  describe SimpleResourcesController do
-    describe 'RESTful actions do their thing' do
-      def valid_attributes
-        {}
-      end
+describe SimpleResourcesController do
+  describe 'DRYs up RESTful scaffolded actions' do
+    def valid_attributes
+      {}
+    end
 
-      def valid_session
-        {}
-      end
+    def valid_session
+      {}
+    end
 
-      describe "GET index" do
-        it "assigns all simple resources as collection" do
-          pending ('need to fix RoutingError')
-          r = SimpleResource.create! valid_attributes
-          get :index, {}, valid_session
-          assigns(:collection).should eq([r])
+    describe "GET index" do
+      it "assigns all simple resources as collection" do
+        r = SimpleResource.create! valid_attributes
+        get :index, {}, valid_session
+        assigns(:collection).should eq([r])
+      end
+    end
+
+    describe "GET show" do
+      it "assigns the requested resource as resource" do
+        r = SimpleResource.create! valid_attributes
+        get :show, {:id => r.to_param}, valid_session
+        assigns(:resource).should eq(r)
+      end
+    end
+
+    describe "GET new" do
+      it "assigns a new resource as resource" do
+        get :new, {}, valid_session
+        assigns(:resource).should be_a_new(SimpleResource)
+      end
+    end
+
+    describe "GET edit" do
+      it "assigns the requested resource as resource" do
+        r = SimpleResource.create! valid_attributes
+        get :edit, {:id => r.to_param}, valid_session
+        assigns(:resource).should eq(r)
+      end
+    end
+
+    describe "POST create" do
+      describe "with valid params" do
+        it "creates a new resource" do
+          expect {
+            post :create, {:simple_resource => valid_attributes}, valid_session
+          }.to change(SimpleResource, :count).by(1)
+        end
+
+        it "assigns a newly created resource as resource" do
+          post :create, {:simple_resource => valid_attributes}, valid_session
+          assigns(:resource).should be_a(SimpleResource)
+          assigns(:resource).should be_persisted
+        end
+
+        it "redirects to the created resource" do
+          post :create, {:simple_resource => valid_attributes}, valid_session
+          response.should redirect_to(SimpleResource.last)
         end
       end
 
-      describe "GET show" do
-        it "assigns the requested resource as resource" do
-          pending ('need to fix RoutingError')
-          r = SimpleResource.create! valid_attributes
-          get :show, {:id => r.to_param}, valid_session
-          assigns(:resource).should eq(r)
-        end
-      end
-
-      describe "GET new" do
-        it "assigns a new resource as resource" do
-          pending ('need to fix RoutingError')
-          get :new, {}, valid_session
+      describe "with invalid params" do
+        it "assigns a newly created but unsaved resource as resource" do
+          # Trigger the behavior that occurs when invalid params are submitted
+          SimpleResource.any_instance.stub(:save).and_return(false)
+          post :create, {:simple_resource => {}}, valid_session
           assigns(:resource).should be_a_new(SimpleResource)
         end
-      end
 
-      describe "GET edit" do
-        it "assigns the requested resource as resource" do
-          pending ('need to fix RoutingError')
+        it "re-renders the 'new' template" do
+          pending("Failure/Error: response.should render_template('new')")
+          # Trigger the behavior that occurs when invalid params are submitted
+          SimpleResource.any_instance.stub(:save).and_return(false)
+          post :create, {:simple_resource => {}}, valid_session
+          response.should render_template("new")
+        end
+      end
+    end
+
+    describe "PUT update" do
+      describe "with valid params" do
+        it "updates the requested resource" do
           r = SimpleResource.create! valid_attributes
-          get :edit, {:id => r.to_param}, valid_session
+          # Assuming there are no other resources in the database, this
+          # specifies that the resource created on the previous line
+          # receives the :update_attributes message with whatever params are
+          # submitted in the request.
+          SimpleResource.any_instance.should_receive(:update_attributes).with({'these' => 'params'})
+          put :update, {:id => r.to_param, :simple_resource => {'these' => 'params'}}, valid_session
+        end
+
+        it "assigns the requested resource as resource" do
+          r = SimpleResource.create! valid_attributes
+          put :update, {:id => r.to_param, :simple_resource => valid_attributes}, valid_session
           assigns(:resource).should eq(r)
         end
-      end
 
-      describe "POST create" do
-        describe "with valid params" do
-          it "creates a new resource" do
-            pending ('need to fix RoutingError')
-            expect {
-              post :create, {:simple_resource => valid_attributes}, valid_session
-            }.to change(SimpleResource, :count).by(1)
-          end
-
-          it "assigns a newly created resource as resource" do
-            pending ('need to fix RoutingError')
-            post :create, {:simple_resource => valid_attributes}, valid_session
-            assigns(:resource).should be_a(SimpleResource)
-            assigns(:resource).should be_persisted
-          end
-
-          it "redirects to the created resource" do
-            pending ('need to fix RoutingError')
-            post :create, {:simple_resource => valid_attributes}, valid_session
-            response.should redirect_to(SimpleResource.last)
-          end
-        end
-
-        describe "with invalid params" do
-          it "assigns a newly created but unsaved resource as resource" do
-            pending ('need to fix RoutingError')
-            # Trigger the behavior that occurs when invalid params are submitted
-            SimpleResource.any_instance.stub(:save).and_return(false)
-            post :create, {:simple_resource => {}}, valid_session
-            assigns(:resource).should be_a_new(SimpleResource)
-          end
-
-          it "re-renders the 'new' template" do
-            pending ('need to fix RoutingError')
-            # Trigger the behavior that occurs when invalid params are submitted
-            SimpleResource.any_instance.stub(:save).and_return(false)
-            post :create, {:simple_resource => {}}, valid_session
-            response.should render_template("new")
-          end
-        end
-      end
-
-      describe "PUT update" do
-        describe "with valid params" do
-          it "updates the requested resource" do
-            pending ('need to fix RoutingError')
-            r = SimpleResource.create! valid_attributes
-            # Assuming there are no other resources in the database, this
-            # specifies that the resource created on the previous line
-            # receives the :update_attributes message with whatever params are
-            # submitted in the request.
-            SimpleResource.any_instance.should_receive(:update_attributes).with({'these' => 'params'})
-            put :update, {:id => r.to_param, :simple_resource => {'these' => 'params'}}, valid_session
-          end
-
-          it "assigns the requested resource as resource" do
-            pending ('need to fix RoutingError')
-            r = SimpleResource.create! valid_attributes
-            put :update, {:id => r.to_param, :simple_resource => valid_attributes}, valid_session
-            assigns(:resource).should eq(r)
-          end
-
-          it "redirects to the resource" do
-            pending ('need to fix RoutingError')
-            r = SimpleResource.create! valid_attributes
-            put :update, {:id => r.to_param, :simple_resource => valid_attributes}, valid_session
-            response.should redirect_to(r)
-          end
-        end
-
-        describe "with invalid params" do
-          it "assigns the resource as resource" do
-            pending ('need to fix RoutingError')
-            r = SimpleResource.create! valid_attributes
-            # Trigger the behavior that occurs when invalid params are submitted
-            SimpleResource.any_instance.stub(:save).and_return(false)
-            put :update, {:id => r.to_param, :simple_resource => {}}, valid_session
-            assigns(:resource).should eq(r)
-          end
-
-          it "re-renders the 'edit' template" do
-            pending ('need to fix RoutingError')
-            r = SimpleResource.create! valid_attributes
-            # Trigger the behavior that occurs when invalid params are submitted
-            SimpleResource.any_instance.stub(:save).and_return(false)
-            put :update, {:id => r.to_param, :simple_resource => {}}, valid_session
-            response.should render_template("edit")
-          end
-        end
-      end
-
-      describe "DELETE destroy" do
-        it "destroys the requested resource" do
-          pending ('need to fix RoutingError')
+        it "redirects to the resource" do
           r = SimpleResource.create! valid_attributes
-          expect {
-            delete :destroy, {:id => r.to_param}, valid_session
-          }.to change(SimpleResource, :count).by(-1)
+          put :update, {:id => r.to_param, :simple_resource => valid_attributes}, valid_session
+          response.should redirect_to(r)
+        end
+      end
+
+      describe "with invalid params" do
+        it "assigns the resource as resource" do
+          r = SimpleResource.create! valid_attributes
+          # Trigger the behavior that occurs when invalid params are submitted
+          SimpleResource.any_instance.stub(:save).and_return(false)
+          put :update, {:id => r.to_param, :simple_resource => {}}, valid_session
+          assigns(:resource).should eq(r)
         end
 
-        it "redirects to the resources list" do
-          pending ('need to fix RoutingError')
+        it "re-renders the 'edit' template" do
+          pending("Failure/Error: response.should render_template('edit')")
           r = SimpleResource.create! valid_attributes
-          delete :destroy, {:id => r.to_param}, valid_session
-          response.should redirect_to(resources_url)
+          # Trigger the behavior that occurs when invalid params are submitted
+          SimpleResource.any_instance.stub(:save).and_return(false)
+          put :update, {:id => r.to_param, :simple_resource => {}}, valid_session
+          response.should render_template("edit")
         end
-      end      
+      end
     end
+
+    describe "DELETE destroy" do
+      it "destroys the requested resource" do
+        r = SimpleResource.create! valid_attributes
+        expect {
+          delete :destroy, {:id => r.to_param}, valid_session
+        }.to change(SimpleResource, :count).by(-1)
+      end
+
+      it "redirects to the resources list" do
+        pending("undefined local variable or method 'collection_path'")
+        r = SimpleResource.create! valid_attributes
+        delete :destroy, {:id => r.to_param}, valid_session
+        response.should redirect_to(collection_url)
+      end
+    end      
   end
 end

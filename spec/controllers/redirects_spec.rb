d782eaf03776f9e09c5826e3dad2bbdbc2b25e83
Mark Ratjens <mark@ratjens.com>
2013-01-24 18:34:19 +1100

    test that you can override redirects

--- spec/controllers/redirects_spec.rb
+++ spec/controllers/redirects_spec.rb
@@ -1,25 +1,39 @@
 require 'spec_helper'
 
 describe 'In the context of generic RESTful-ness,' do
-  describe SimpleResourcesController do
+  let(:ggp)       { GreatGrandparentResource.create! }
+  let(:gp)        { GrandparentResource.create!(:great_grandparent_resource => ggp) }
+
+  describe GrandparentResourcesController do
     describe 'overriding redirects' do
-      describe "POST create" do
-        it "redirects as adapted" do
-          pending
-        end
+      it "redirects from create as adapted" do
+        expect {
+          post :create, {:great_grandparent_resource_id => ggp.id}
+        }.to change(GreatGrandparentResource, :count).by(1)
+
+        subject.send(:resource).should be_a(GrandparentResource)
+        subject.send(:resource).should be_persisted
+
+        response.should redirect_to(subject.send(:great_grandparent_resource_url, ggp))
       end
 
-      describe "PUT update" do
-        it "redirects as adapted" do
-          pending
-        end
+      it "redirects from update as adapted" do
+        GrandparentResource.any_instance.should_receive(:update_attributes).with({'these' => 'params'})
+        put :update, {:id => gp.to_param, :grandparent_resource => {'these' => 'params'}, :great_grandparent_resource_id => ggp.id}
+
+        subject.send(:resource).should eq(gp)
+
+        response.should redirect_to(subject.send(:great_grandparent_resource_url, ggp))
       end
 
-      describe "DELETE destroy" do
-        it "redirects as adapted" do
-          pending
-        end
-      end      
+      it "redirects from destroy as adapted" do
+        d = GrandparentResource.create!(:great_grandparent_resource => ggp)
+        expect {
+          delete :destroy, {:id => d.to_param, :great_grandparent_resource_id => ggp.id}
+        }.to change(GrandparentResource, :count).by(-1)
+
+        response.should redirect_to(subject.send(:great_grandparent_resource_url, ggp))
+      end
     end
   end
 end
--- spec/dummy/app/controllers/grandparent_resources_controller.rb
+++ spec/dummy/app/controllers/grandparent_resources_controller.rb
@@ -1,2 +1,13 @@
 class GrandparentResourcesController < ApplicationController
+	def create
+		super :location => polymorphic_url(parent)
+	end
+
+	def update
+		super :location => polymorphic_url(parent)
+	end
+
+	def destroy
+		super :location => polymorphic_url(parent)
+	end
 end

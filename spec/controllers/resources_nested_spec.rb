49f7d34c12bd5beaa665f87431a7939a3af5cc8a
Mark Ratjens <mark@ratjens.com>
2013-01-24 18:45:27 +1100

    test methods named after objects in the association chain

--- spec/controllers/resources_nested_spec.rb
+++ spec/controllers/resources_nested_spec.rb
@@ -1,27 +1,37 @@
 require 'spec_helper'
 
-describe 'some nested controllers' do
+describe ChildResourcesController do
+  let(:ggp)       { GreatGrandparentResource.create! }
+  let(:gp)        { GrandparentResource.create!(:great_grandparent_resource => ggp) }
+  let(:p)         { ParentResource.create!(:grandparent_resource => gp) }
+  let(:c)         { ChildResource.create!(:parent_resource => p) }
+
   describe 'parent method' do
     it "answers the resource's immediate parent through a standard parent method" do
-      pending
+      get :show, {:id => c.to_param, :parent_resource_id => p.id, :grandparent_resource_id => gp.id, :great_grandparent_resource_id => ggp.id}
+      subject.send(:parent).should eq(p)
     end
   end
 
   describe 'nested resources by derived name methods' do
     it "answers the resource through a method named after the route information" do
-      pending
+      get :show, {:id => c.to_param, :parent_resource_id => p.id, :grandparent_resource_id => gp.id, :great_grandparent_resource_id => ggp.id}
+      subject.send(:child_resource).should eq(c)
     end
 
     it "answers the resource's parent through a method named after the route information" do
-      pending
+      get :show, {:id => c.to_param, :parent_resource_id => p.id, :grandparent_resource_id => gp.id, :great_grandparent_resource_id => ggp.id}
+      subject.send(:parent_resource).should eq(p)
     end
 
     it "answers the resource's grandparent through a method named after the route information" do
-      pending
+      get :show, {:id => c.to_param, :parent_resource_id => p.id, :grandparent_resource_id => gp.id, :great_grandparent_resource_id => ggp.id}
+      subject.send(:grandparent_resource).should eq(gp)
     end
 
     it "answers the resource's great-grandparent through a method named after the route information" do
-      pending
+      get :show, {:id => c.to_param, :parent_resource_id => p.id, :grandparent_resource_id => gp.id, :great_grandparent_resource_id => ggp.id}
+      subject.send(:great_grandparent_resource).should eq(ggp)
     end
   end
 end

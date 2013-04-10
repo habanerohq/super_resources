5bb7bd6487588cf6c33310f3a60a2cf65939a442
Ben Caldwell <bencaldwell@einti.com>
2013-04-10 16:45:03 +0800

    Let ActiveSupport::Concern handle dependencies

--- lib/super_resources/controller.rb
+++ lib/super_resources/controller.rb
@@ -2,11 +2,9 @@ module SuperResources
   module Controller
     extend ActiveSupport::Concern
 
-    included do
-    	include Actions
-	    include Nesting
-	    include HasScope
-	    include URLHelpers	    
-	  end
+    include Actions
+    include Nesting
+    include HasScope
+    include URLHelpers
   end
 end

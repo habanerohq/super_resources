ce11b9a48a395910007d155c26b79fbb57f3f36d
Mark Ratjens <mark@ratjens.com>
2013-09-20 10:31:42 +1000

    handle urls that map to controllers inside modules

--- lib/super_resources/url_helpers.rb
+++ lib/super_resources/url_helpers.rb
@@ -22,14 +22,6 @@ module SuperResources
 
     def super_path(chain, options={})
       super_url(chain, options.merge(:routing_type => :path))
-=begin
-      polymorphic_url(chain, options.merge(:routing_type => :path))
-    rescue NoMethodError => e
-      object = chain.pop
-
-      chain.empty? ?
-        raise(e) : super_path(chain.slice(0...-1) << object, options)
-=end
     end
 
     def super_url(chain, options={})
@@ -44,11 +36,11 @@ module SuperResources
     # collection route helpers .................................................
 
     def collection_url
-      super_url(with_chain(resource_class))
+      super_url(with_chain(qualified_resource_class))
     end
 
     def collection_path
-      super_path(with_chain(resource_class))
+      super_path(with_chain(qualified_resource_class))
     end
 
     def hash_for_collection_url(options={})
@@ -59,30 +51,30 @@ module SuperResources
 
     def resource_path(*args)
       options = args.extract_options!
-      super_path(with_chain(args.first || resource), options)
+      super_path(with_chain(args.first || qualified_resource), options)
     end
 
     def resource_url(*args)
       options = args.extract_options!
-      super_url(with_chain(args.first || resource), options)
+      super_url(with_chain(args.first || qualified_resource), options)
     end
 
     def hash_for_resource_url(*args)
       options = args.extract_options!
       route_hash.merge(options)
-                .merge(:action => 'show', :id => args.first || resource)
+                .merge(:action => 'show', :id => args.first || qualified_resource)
     end
 
     # new resource route helpers ...............................................
 
     def new_resource_path(options={})
       options.merge! :action => :new
-      super_path(with_chain(resource_class), options)
+      super_path(with_chain(qualified_resource_class), options)
     end
 
     def new_resource_url(options={})
       options.merge! :action => :new
-      super_url(with_chain(resource_class), options)
+      super_url(with_chain(qualified_resource_class), options)
     end
 
     def hash_for_new_resource_url(options={})
@@ -95,35 +87,53 @@ module SuperResources
       options = args.extract_options!
       options.merge! :action => :edit
 
-      super_path(with_chain(args.first || resource), options)
+      super_path(with_chain(args.first || qualified_resource), options)
     end
 
     def edit_resource_url(*args)
       options = args.extract_options!
       options.merge! :action => :edit
 
-      super_url(with_chain(args.first || resource), options)
+      super_url(with_chain(args.first || qualified_resource), options)
     end
 
     def hash_for_edit_resource_url(*args)
       options = args.extract_options!
       route_hash.merge(options)
-                .merge(:action => 'edit', :id => args.first || resource)
+                .merge(:action => 'edit', :id => args.first || qualified_resource)
     end
 
     # parent path helpers ......................................................
 
     def parent_path(options={})
-      super_path(association_chain, options)
+      super_path(qualified_association_chain, options)
     end
 
     def parent_url(options={})
-      super_path(association_chain, options)
+      super_path(qualified_association_chain, options)
     end
 
     def hash_for_parent_path(options={})
       # TODO: mess around with this sucker
       raise NotImplementedException
+    end
+
+    # module qualification helpers .............................................
+
+    def qualified_resource
+      (module_name_hierarchy << resource).compact
+    end
+
+    def qualified_resource_class
+      (module_name_hierarchy << resource_class).compact
+    end
+
+    def qualified_association_chain
+      (module_name_hierarchy + association_chain).compact
+    end
+
+    def module_name_hierarchy
+      self.class.name.split('::')[0 .. -2].map(&:underscore)
     end
   end
 end
